c3cacd574ee8fef4f95ed93d11e3788ab737e0b2
Vadim Samarin <dezivert@gmail.com>
2024-04-14 21:23:47 +0300

    spawn rose

--- src/Roses.gd
+++ src/Roses.gd
@@ -1,5 +1,6 @@
 extends Interactable
 
+var S = preload('res://rose.tscn')
 
 func get_interactable_root():
 	return self
@@ -7,6 +8,11 @@ func get_interactable_root():
 
 
 
-func interact(issuer):
+func interact(_issuer):
+	var s = S.instantiate();
+	get_tree().root.get_node('Simulation').add_child(s)
+	s.position = self.global_position + Vector2(-100, 250)
 
-	pass
+func is_interactable(_issuer):
+	var existsing = get_tree().get_nodes_in_group("roses")
+	return existsing.size() < 1 
